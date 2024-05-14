5131b3143653f74e284196a2476cb31a669b0882
Ilia <isinetov@mail.ru>
2024-05-15 02:05:21 +0400

    Задание 14.1.6.
    Сделайте выборку всех чисел в новую коллекцию, расположив их в ней по возрастанию.
    
    Результат выведите в консоль.

---
+++ Task14.1.6/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program 
+{
+    static void Main(string[] args) 
+    {
+        var numsList = new List<int[]>()
+        {
+            new[] {2, 3, 7, 1},
+            new[] {45, 17, 88, 0},
+            new[] {23, 32, 44, -6},
+        };
+
+        var ascendNums = numsList.SelectMany(s => s).OrderBy(num => num);
+
+        foreach (var num in ascendNums) 
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
