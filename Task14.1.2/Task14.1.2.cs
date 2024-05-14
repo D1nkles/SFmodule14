22846be6436c017b9241e63466ef42b7e1747a3d
Ilia <isinetov@mail.ru>
2024-05-15 00:10:45 +0400

    Задание 14.1.2.
    Соедините все слова в одну последовательность (каждое слово — отдельный элемент последовательности).
    
    Используйте синтаксис LINQ-выражений.

---
+++ Task14.1.2/Task14.1.2.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string[] text = { "Раз два три",
+                          "четыре пять шесть",
+                          "семь восемь девять" };
+
+        var sequence = from sentence in text from word in sentence.Split(" ") select word;
+        foreach (var word in sequence)
+        {
+            Console.Write(word);
+        }
+
+    }
+}
