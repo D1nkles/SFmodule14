4274826429c5869ad7becf31075602df49a64f70
Ilia <isinetov@mail.ru>
2024-05-14 22:35:02 +0400

    Добавьте файлы проекта.

---
+++ Task8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        var objects = new List<object>()
+        {
+            1,
+            "Сергей ",
+            "Андрей ",
+            300,
+        };
+
+        var names = from object obj in objects where obj.GetType() == typeof(string) orderby obj select obj;
+
+        foreach (string name in names) 
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
