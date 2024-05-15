b3ac0f35ccede307b78e90f39000269778a7f31b
Ilia <isinetov@mail.ru>
2024-05-15 17:08:27 +0400

    Задание 14.2.1 Дан массив слов:
    string [] words = { "Обезьяна", "Лягушка", "Кот", "Собака", "Черепаха"};
    Сделайте выборку в анонимный тип с одновременной сортировкой слов по длине. Результат выведите в консоль.

---
+++ Task14.2.1/Task14.2.1.cs
@@ -0,0 +1,19 @@
+﻿class Program 
+{
+    static void Main(string[] args) 
+    {
+        string[] words = { "Обезьяна", "Лягушка", "Кот", "Собака", "Черепаха" };
+        var sortedWords = words.Select(s =>
+        new
+        {
+            Name = s,
+            Length = s.Length
+        })
+            .OrderBy(s => s.Length);
+
+        foreach (var word in sortedWords) 
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
