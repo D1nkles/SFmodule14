37327c0132e67fbd016dc56288876fe77fada1f3
Ilia <isinetov@mail.ru>
2024-05-15 18:36:18 +0400

    Задание 14.2.3
    Выберите всех студентов моложе 27, сгенерируйте из них анкеты (модель класса расположена ниже).

---
+++ Task14.2.3/Task14.2.3.cs
@@ -0,0 +1,38 @@
+﻿class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public List<string> Languages { get; set; }
+
+}
+public class Application
+{
+    public string Name { get; set; }
+    public int YearOfBirth { get; set; }
+}
+class Program
+{
+    static void Main() 
+    {
+        List<Student> students = new List<Student>
+    {
+        new Student { Name = "Андрей", Age = 23, Languages = new List<string> { "английский", "немецкий" } },
+        new Student { Name = "Сергей", Age = 27, Languages = new List<string> { "английский", "французский" } },
+        new Student { Name = "Дмитрий", Age = 29, Languages = new List<string> { "английский", "испанский" } },
+        new Student { Name = "Василий", Age = 24, Languages = new List<string> { "испанский", "немецкий" } }
+    };
+
+        var applications = from student in students 
+                          where student.Age < 27
+                          let yearOfBirth = DateTime.Now.Year - student.Age
+                          select new Application()
+                          {
+                              Name = student.Name,
+                              YearOfBirth = yearOfBirth
+                          };
+        foreach (Application app in applications) 
+        {
+            Console.WriteLine(app.Name + "\t" + app.YearOfBirth);
+        }
+    }
+}
